2a5ca87144a510e458e227893ff7bec8defdbf6d
leconstantin <lcon69184@gmail.com>
2024-12-28 16:03:36 +0200

    last on front

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -27,7 +27,7 @@ export default function MobileMenu() {
         ></div>
       </div>
       {isOpen && (
-        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-20">
+        <div className="fixed left-0 top-24   w-screen min-h-screen z-50 h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl">
           <Link href="">Home</Link>
           <Link href="">Friends</Link>
           <Link href="">Groups</Link>
